20ac56f78e1d7b68f901774ae9e1d8dd27732dba
leesami37 <samikshyakunwar42@gmail.com>
2024-06-01 17:24:17 +0545

    Sample Project

---
+++ reactappbysamikshya/src/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h2> ReactAppBySamikshya</h2>
+      <img src="/Samikshya.jpg" alt="Samikshya" style={{width: "300px"}} />
+    </div>
+  );
+}
+export default Home;
